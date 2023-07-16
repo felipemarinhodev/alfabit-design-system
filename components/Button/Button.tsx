08cfdfc7986e7eae275cfbdb44f104ff88bab669
Felipe Marinho <github@felipemarinho.dev>
2023-07-15 21:05:37 -0700

    a4.5 - secondary button - set style by variant

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,28 +1,48 @@
 export type ButtonProps = {
   children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'tertiary';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>
+
+function getVariant(variant: ButtonProps['variant'], disabled: ButtonProps['disabled']) {
+  let variantStyle = ''
+  switch (variant) {
+    case 'primary':
+      variantStyle = 'bg-primary text-white'
+      break;
+    case 'secondary':
+      variantStyle = 'bg-secondary text-white'
+      break;
+    case 'tertiary':
+      variantStyle = ''
+      break;
+    default:
+      variantStyle = 'bg-primary text-white'
+      break;
+  }
+  return disabled ? 'bg-disabled text-disabled' : variantStyle;
+}
 
 const Button = ({
   children,
   className,
   disabled,
+  variant = 'primary',
   ...props
 }: ButtonProps) => {
-  const generalStyle = 'rounded-md px-6 py-2xs';
-  const Btn = (classes: string) => {
-    return (
-      <button
-        className={`${generalStyle} ${classes} ${className}`}
-        disabled={disabled}
-        {...props}
-      >
-        {children}
-      </button>
-    )
-  }
-  return Btn(disabled
-    ? 'bg-disabled text-disabled'
-    : 'bg-primary text-white')
+
+  return (
+    <button
+      className={`
+        rounded-md px-6 py-2xs 
+        ${getVariant(variant, disabled)}
+        ${className}
+      `}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button;
