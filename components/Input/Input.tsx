a81aaa62eef5b2732a8e92cfd7c1433bb12fd043
Felipe Marinho <github@felipemarinho.dev>
2023-07-16 20:58:04 -0700

    a5.7 - finalizing the documentation - add spaces

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -20,12 +20,13 @@ const InputOrTextArea = ({ multiline = false, ...rest }: InputProps) => {
 const Input = ({ label, className, ...rest }: InputProps) => {
   return (
     <div className="relative">
-      {label && <label className="absolute">{label}</label>}
+      {label && <label className="absolute pt-1 pl-3 text-xs">{label}</label>}
       <InputOrTextArea
         {...rest}
         className={`
-          bg-dark text-gray-primary
+          bg-dark text-gray-primary rounded-md p-3
           disabled:bg-disabled disabled:border-2 disabled:border-disabled
+          ${label && 'pt-5'}
           ${className}
         `}
       />
