af2a7e4c5f0256db9791a5bb99f841243ca01038
Felipe Marinho <github@felipemarinho.dev>
2023-07-23 09:58:05 -0700

    a2.5 - finishing style of the Box - adding new stories

--- components/Box/Box.stories.ts
+++ components/Box/Box.stories.ts
@@ -5,21 +5,72 @@ const meta: Meta<BoxProps> = {
   title: "Atoms/Box",
   component: Box,
   argTypes: {
-    className: {
-      type: "string"
-    }
+    className: { type: "string" },
+    children: { type: "string" },
+    type: { type: "string" },
+    rounded: { type: "boolean" },
+    border: { type: "boolean" },
+    filledBackground: { type: "boolean" },
   }
 }
 
 export default meta;
 
 export const Primary: StoryObj<BoxProps> = {
-  args: {
-  }
+  args: { }
 }
 
-export const WithText: StoryObj<BoxProps> = {
+export const TypePrimary: StoryObj<BoxProps> = {
   args: {
-    children: "Text"
+    children: "Primary",
+    type: 'primary',
+    border: true,
+    rounded: true,
+    filledBackground: true,
+  }
+}
+export const TypeSecondary: StoryObj<BoxProps> = {
+  args: {
+    children: "Secondary",
+    type: 'secondary',
+    border: true,
+    rounded: true,
+    filledBackground: true,
+  }
+}
+export const TypeDark: StoryObj<BoxProps> = {
+  args: {
+    children: "Dark",
+    type: 'dark',
+    border: true,
+    rounded: true,
+    filledBackground: true,
+  }
+}
+export const TypeSuccess: StoryObj<BoxProps> = {
+  args: {
+    children: "Success",
+    type: 'success',
+    border: true,
+    rounded: true,
+    filledBackground: true,
+  }
+}
+export const TypeAlert: StoryObj<BoxProps> = {
+  args: {
+    children: "Alert",
+    type: 'alert',
+    border: true,
+    rounded: true,
+    filledBackground: true,
+  }
+}
+export const TypeError: StoryObj<BoxProps> = {
+  args: {
+    children: "Error",
+    type: 'error',
+    border: true,
+    rounded: true,
+    filledBackground: true,
   }
 }
