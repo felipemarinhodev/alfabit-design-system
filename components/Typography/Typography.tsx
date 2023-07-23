a5cbb7f5b07e198f29a7ba0fa72e24c35b2852f8
Felipe Marinho <github@felipemarinho.dev>
2023-07-23 08:09:29 -0700

    a1.6 - using CVA - using the CVA to change the styles

--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -1,41 +1,53 @@
-export type TypographyProps = {
-  variant?: 'primary' | 'secondary' | 'tertiary';
-  size?: 'xs' | 'sm' | 'md' | 'xl' | 'title1' | 'title2' | 'title3';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+const textVariants = cva('font-normal', {
+  variants: {
+    variant: {
+      primary: 'text-gray-primary',
+      secondary: 'text-gray-secondary',
+      tertiary: 'text-gray-tertiary',
+    },
+    size: {
+      xs: 'text-xs',
+      sm: 'text-sm',
+      md: 'text-md',
+      xl: 'text-xl',
+      title1: 'text-txl',
+      title2: 'text-tlg',
+      title3: 'text-tmd',
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md'
+    }
+  }
+})
+
+export type TypographyProps = VariantProps<typeof textVariants> & {
+  // This should be omitted because the CVA is now responsible for adding these props.
+  // variant?: 'primary' | 'secondary' | 'tertiary';
+  // size?: 'xs' | 'sm' | 'md' | 'xl' | 'title1' | 'title2' | 'title3';
   children: React.ReactNode;
   element?: keyof JSX.IntrinsicElements;
 } & JSX.IntrinsicElements['p'];
 
 const variantClassMap = {
-  variant: {
-    primary: 'text-gray-primary',
-    secondary: 'text-gray-secondary',
-    tertiary: 'text-gray-tertiary',
-  },
-  size: {
-    xs: 'text-xs',
-    sm: 'text-sm',
-    md: 'text-md',
-    xl: 'text-xl',
-    title1: 'text-txl',
-    title2: 'text-tlg',
-    title3: 'text-tmd',
-  }
+
 };
 
 const Typography = ({
-  variant = 'primary',
-  size = 'md',
+  // This should be omitted because the CVA is now responsible for adding these props.
+  // variant = 'primary',
+  // size = 'md',
   children,
   element = "p",
+  className,
   ...rest
 }: TypographyProps) => {
   const Element = element as any;
   return (
     <Element
-      className={`
-        font-normal
-        ${variantClassMap.variant[variant]}
-        ${variantClassMap.size[size]}`}
+      className={`${textVariants(rest)} ${className}`}
       {...rest}
     >
       {children}
