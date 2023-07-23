0e3f4bbafa52bf37f42c4310f1e7eabc709a8e04
Felipe Marinho <github@felipemarinho.dev>
2023-07-23 09:04:32 -0700

    a2.3 - using classnames - using classnames and adding consts to handle the styles

--- components/Divider/Divider.tsx
+++ components/Divider/Divider.tsx
@@ -1,3 +1,5 @@
+import classNames from "classnames";
+
 export type DividerPros = {
   width?: string;
   height?: string;
@@ -5,12 +7,33 @@ export type DividerPros = {
   children?: React.ReactNode;
 } & React.HtmlHTMLAttributes<HTMLDivElement>;
 
-const Divider = ({ width, height, bgColor, children, ...rest }: DividerPros) => {
+const colorClassMap = {
+  light: 'bg-light',
+  dark: 'bg-dark',
+  black: 'bg-black',
+}
+
+const Divider = ({
+  width,
+  height = 'h-[1px]',
+  bgColor = 'black',
+  children,
+  ...rest
+}: DividerPros) => {
+  const colorClass = colorClassMap[bgColor];
+  const barClass = classNames(
+    children ? 'w-1/3' : 'w-1/2',
+    height,
+    colorClass
+  );
   return (
-    <div>
-      <div></div>
-      {children && <div>{children}</div>}
-      <div></div>
+    <div
+      className={classNames(width, "flex items-center justify-center")}
+      {...rest}
+    >
+      <div className={barClass}></div>
+      {children && <div className="px-3">{children}</div>}
+      <div className={barClass}></div>
     </div>
   )
 }
