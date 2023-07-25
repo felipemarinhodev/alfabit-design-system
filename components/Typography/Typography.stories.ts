d3b864fc236953369ed73eef53372e8a5e039fe4
Felipe Marinho <github@felipemarinho.dev>
2023-07-24 20:43:47 -0700

    a5.2 - update doc - update `Typography` docs

--- components/Typography/Typography.stories.ts
+++ components/Typography/Typography.stories.ts
@@ -18,3 +18,39 @@ export const Primary: StoryObj<TypographyProps> = {
     children: "Text"
   }
 }
+
+export const TextHeading1: StoryObj<TypographyProps> = {
+  args: {
+    children: "Heading 1",
+    element: 'h1',
+    size: 'title1',
+    className: 'font-extrabold'
+  }
+}
+
+export const TextHeading2: StoryObj<TypographyProps> = {
+  args: {
+    children: "Heading 2",
+    element: 'h2',
+    size: 'title2',
+    className: 'font-extrabold'
+  }
+}
+
+export const TextHeading3: StoryObj<TypographyProps> = {
+  args: {
+    children: "Heading 3",
+    element: 'h3',
+    size: 'title3',
+    className: 'font-extrabold'
+  }
+}
+
+export const TextParagraph: StoryObj<TypographyProps> = {
+  args: {
+    children: "Paragraph",
+    element: 'p',
+    size: 'md',
+    className: 'font-primary'
+  }
+}
